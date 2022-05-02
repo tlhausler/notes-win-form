dbf568d629c0ab197df2221d36c053c18b5aa794
Tamber Hausler <tamber.hausler@mail.helenacollege.edu>
2022-05-01 23:13:48 -0600

    Added comments

--- NotesApp/Form1.cs
+++ NotesApp/Form1.cs
@@ -22,11 +22,14 @@ namespace NotesApp
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            // instantiate Data Table and add columns
+            // Set noteTable as data source for data grid view on form
             noteTable = new DataTable();
             noteTable.Columns.Add("Title", typeof(String));
             noteTable.Columns.Add("Notes", typeof(String));
             dgvNoteList.DataSource = noteTable;
 
+            // formatting the data grid view
             dgvNoteList.Columns["Notes"].Visible = false;
             dgvNoteList.Columns["Title"].Width = 245;
 
@@ -34,6 +37,7 @@ namespace NotesApp
 
         private void btnSave_Click(object sender, EventArgs e)
         {
+            // save note to data grid view and clear the title and note boxes
             noteTable.Rows.Add(txtBoxTitle.Text, txtBoxNote.Text);
             txtBoxTitle.Clear();
             txtBoxNote.Clear();
@@ -41,12 +45,14 @@ namespace NotesApp
 
         private void btnNew_Click(object sender, EventArgs e)
         {
+            // clear title and note text boxes for creating new note
             txtBoxTitle.Clear();
             txtBoxNote.Clear();
         }
 
         private void btnEdit_Click(object sender, EventArgs e)
         {
+            // retrieve data from table and place in title and note text fields
             int index = dgvNoteList.CurrentCell.RowIndex;
 
             if (index > -1)
@@ -58,6 +64,7 @@ namespace NotesApp
 
         private void btnDelete_Click(object sender, EventArgs e)
         {
+            // delete selected note from table by index
             int index = dgvNoteList.CurrentCell.RowIndex;
 
             noteTable.Rows[index].Delete();
